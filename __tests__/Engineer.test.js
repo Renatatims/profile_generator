d05259fe85f146e61584edc66a550a04880faec9
ReTims <renatatims@gmail.com>
2022-10-04 11:33:38 -0500

    Testing Engineer's class - added 3 tests

--- __tests__/Engineer.test.js
+++ __tests__/Engineer.test.js
@@ -1,9 +1,28 @@
 const TestEngineer = require ('../lib/Engineer');
 
+//Testing Engineer's object
 describe ('TestEngineer', () => {
-    it ("should create an object with name, id, email and github profile", () => {
-    const name = "Engineer Name"
-    const obj = new TestEngineer(name);
-    expect (obj.name).toEqual (name);
+    it ("should create a new engineer object", () => {
+    const obj = new TestEngineer();
+    expect (typeof(obj)).toBe("object");
+})
+});
+
+
+//Testing Engineer's getGitHub()
+describe ("Test Engineer's gitHub", () => {
+    it ("should create an object with a GitHub account", () => {
+    const github = "engineerGitHub";
+    const obj = new TestEngineer("Employee Name", 10, "employee@gmail.com", github);
+    expect (obj.github).toEqual(github);
+})
+});
+
+// Testing Engineer's Role
+describe ("Test Engineer's role", () => {
+    it ("should select the Engineer's role", () => {
+    const engineer = "Engineer";
+    const obj = new TestEngineer("Employee Name", 10, "employee@gmail.com", "engineerGitHub");
+    expect (obj.getRole()).toEqual(engineer);
 })
 });
