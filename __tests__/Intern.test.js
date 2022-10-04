56a7d84895df60e47c57d7261bafc7cd51e7f1c7
ReTims <renatatims@gmail.com>
2022-10-04 11:36:52 -0500

    Testing Inter's class - added 3 tests

--- __tests__/Intern.test.js
+++ __tests__/Intern.test.js
@@ -1,9 +1,28 @@
 const TestIntern = require ('../lib/Intern');
 
+//Testing Intern's object
 describe ('TestIntern', () => {
-    it ("should create an object with name, id, email and school", () => {
-    const name = "Intern Name"
-    const obj = new TestIntern(name);
-    expect (obj.name).toEqual (name);
+    it ("should create a new Intern object", () => {
+    const obj = new TestIntern();
+    expect (typeof(obj)).toBe("object");
+})
+});
+
+
+//Testing Intern's getSchool()
+describe ("Test Intern's school", () => {
+    it ("should create an object with intern's school", () => {
+    const school = "School";
+    const obj = new TestIntern("Employee Name", 10, "employee@gmail.com", school);
+    expect (obj.school).toEqual(school);
+})
+});
+
+// Testing Intern's Role
+describe ("Test Intern's role", () => {
+    it ("should select the Intern's role", () => {
+    const Intern = "Intern";
+    const obj = new TestIntern("Employee Name", 10, "employee@gmail.com", "School");
+    expect (obj.getRole()).toEqual(Intern);
 })
 });
